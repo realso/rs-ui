e7c4edd7c48e4a8c23107bed8a0632a223823288
realso <284436863@qq.com>
2018-11-09 14:21:08 +0800

    2

--- src/packages/grid/src/grid.vue
+++ src/packages/grid/src/grid.vue
@@ -1,5 +1,5 @@
 <template>
-  <ul class="mui-table-view mui-grid-view mui-grid-9" :class="{'rs-grid-border':border}">
+  <ul class="mui-table-view mui-grid-view mui-grid-9" :class="'rs-grid-' + type,{'rs-grid-border':border,}">
     <slot></slot>
   </ul>
 </template>
@@ -9,11 +9,11 @@
  * rs-grid
  * @module components/grid
  * @desc 九宫格
- * @param type {string} [type=default] - 显示类型，接受 default, primary, danger,success,white
+ * @param type {string} [type=default] - 显示类型，接受 default(浅灰), gary（深灰）, white（白色）
  * @param border {boolean} [border=false] - 有无边框 默认有边框
  *
  * @example
- * <rs-grid type="primary" border=true>
+ * <rs-grid type="default" border=true>
  *    <rs-grid-item>
  *      <span slot="icon" class="mui-icon mui-icon-home"></span>
  *      内容A
@@ -26,22 +26,14 @@
  */
 export default {
   name: 'rs-grid',
-  methods: {
-    handleClick (evt) {
-      this.$emit('click', evt)
-    }
-  },
   props: {
     border: Boolean,
     type: {
       type: String,
-      default: 'primary',
+      default: 'default',
       validator (value) {
         return [
-          'primary',
-          'danger',
-          'success',
-          'warning',
+          'gary',
           'white'
         ].indexOf(value) > -1
       }
@@ -53,10 +45,20 @@ export default {
 @component-namespace rs {
     @component grid {
       @descendent border {
+        border: none;
         .mui-table-view-cell {
-          border-right: none;
-          border-bottom: none;
+          border-right: none !important;
+          border-bottom: none !important;
         }
+      }
+      @descendent default {
+        background: #f2f2f2;
+      }
+      @descendent gary {
+        background: #ccc;
+      }
+      @descendent white {
+        background: #fff;
       }
     }
   }
